af07fb1c2ebc18e91e3a28a4f35c985e9d1e3dff
Meghan Jennings <meghanjennings@Meghans-MacBook-Pro.local>
2020-10-26 10:37:46 -0400

    completed DB structure implementation

--- hikerite.sql
+++ hikerite.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Oct 19, 2020 at 10:05 PM
+-- Host: 127.0.0.1
+-- Generation Time: Oct 23, 2020 at 01:07 AM
 -- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- PHP Version: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,10 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Hikers`
+-- Table structure for table `check_ins`
 --
 
-CREATE TABLE `Hikers` (
+CREATE TABLE `check_ins` (
+  `hiker_id` int(15) NOT NULL,
+  `trail_id` int(15) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorites`
+--
+
+CREATE TABLE `favorites` (
+  `trail_id` int(11) NOT NULL,
+  `hiker_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hikers`
+--
+
+CREATE TABLE `hikers` (
   `id` int(11) NOT NULL,
   `username` varchar(15) NOT NULL,
   `phone` char(10) DEFAULT NULL,
@@ -35,14 +59,73 @@ CREATE TABLE `Hikers` (
   `photo` varchar(256) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `hiker_id` int(11) NOT NULL,
+  `trail_id` int(11) NOT NULL,
+  `text` varchar(500) DEFAULT NULL,
+  `date` date NOT NULL,
+  `score` enum('1 (terrible)','2','3','4','5 (amazing)') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trails`
+--
+
+CREATE TABLE `trails` (
+  `id` int(11) NOT NULL,
+  `trail_name` varchar(25) NOT NULL,
+  `difficulty` enum('Beginner','Intermediate','Advanced') NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `trail_photo` varchar(256) DEFAULT NULL,
+  `address` varchar(250) NOT NULL,
+  `lat_` varchar(11) NOT NULL,
+  `long_` varchar(11) NOT NULL,
+  `cost` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `Hikers`
+-- Indexes for table `check_ins`
 --
-ALTER TABLE `Hikers`
+ALTER TABLE `check_ins`
+  ADD PRIMARY KEY (`hiker_id`,`trail_id`),
+  ADD KEY `trail_id` (`trail_id`);
+
+--
+-- Indexes for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD PRIMARY KEY (`trail_id`,`hiker_id`),
+  ADD KEY `hiker_id` (`hiker_id`);
+
+--
+-- Indexes for table `hikers`
+--
+ALTER TABLE `hikers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`hiker_id`,`trail_id`),
+  ADD KEY `trail_id` (`trail_id`);
+
+--
+-- Indexes for table `trails`
+--
+ALTER TABLE `trails`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -50,10 +133,35 @@ ALTER TABLE `Hikers`
 --
 
 --
--- AUTO_INCREMENT for table `Hikers`
+-- AUTO_INCREMENT for table `hikers`
 --
-ALTER TABLE `Hikers`
+ALTER TABLE `hikers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `check_ins`
+--
+ALTER TABLE `check_ins`
+  ADD CONSTRAINT `check_ins_ibfk_1` FOREIGN KEY (`hiker_id`) REFERENCES `hikers` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `check_ins_ibfk_2` FOREIGN KEY (`trail_id`) REFERENCES `trails` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`hiker_id`) REFERENCES `hikers` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`trail_id`) REFERENCES `trails` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`hiker_id`) REFERENCES `hikers` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`trail_id`) REFERENCES `trails` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
